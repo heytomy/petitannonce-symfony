8323d59bb11fb558ff3f151e74963d240de8204a
nisitomy <nisi.thomas@outlook.fr>
2022-11-21 11:52:32 +0100

    Add Bootstrap, add routes, controllers

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'title' => 'Bienvenue sur Duckzon',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam cum corrupti modi cupiditate nostrum odit illo veniam, nulla neque officia expedita rerum, aliquid libero incidunt rem iusto reprehenderit maxime!',
+            'date' => new \DateTime(),
+        ]);
+    }
+}
