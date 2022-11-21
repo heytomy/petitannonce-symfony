549e3850dccaf93f18c6457b76c9826f7dbfaad2
nisitomy <nisi.thomas@outlook.fr>
2022-11-21 14:54:33 +0100

    y'a plus rien qui marche

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnnonceController extends AbstractController
 {
-    #[Route('/annonce', name: 'app_annonce')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine)
     {
-        return $this->render('annonce/index.html.twig', [
-            'controller_name' => 'AnnonceController',
-        ]);
+        $repository = $doctrine()->getRepository(Annonce::class);
     }
+    /**
+ * @Route("/annonce/new")
+ */
+public function new(ManagerRegistry $doctrine){
+    $annonce = new Annonce();
+    $annonce
+        ->setTitle('Ma collection de canard vivant')
+        ->setDescription('Vends car plus d\'utilité')
+        ->setPrice(10)
+        ->setStatus(Annonce::STATUS_BAD)
+        ->setSold(false)
+    ;
+
+    // On récupère l'EntityManager 
+    $em = $doctrine->getManager();
+    // On « persiste » l'entité
+    $em->persist($annonce);
+    // On envoie tout ce qui a été persisté avant en base de données
+    $em->flush();
+}
 }
---
+++ src/Entity/Annonce.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnnonceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnnonceRepository::class)]
+class Annonce
+{
+    const STATUS_VERY_BAD  = 0;
+    const STATUS_BAD       = 1;
+    const STATUS_GOOD      = 2;
+    const STATUS_VERY_GOOD = 3;
+    const STATUS_PERFECT   = 4;
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column]
+    private ?bool $sold = false;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+    {
+        $this->sold = $sold;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
